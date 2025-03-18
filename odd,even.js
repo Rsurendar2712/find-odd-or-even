828b1acc3acf9b87f2f5b172fecd5ddbeca6d20b
Rsurendar2712 <surendarramesh94756@gmail.com>
2025-03-18 12:30:27 +0530

    Add files via upload

---
+++ odd,even.js
@@ -0,0 +1,38 @@
+function odd(){  
+    var x=document.getElementById('in').value;
+     var y=document.getElementById('final');
+   
+    var s=document.getElementById
+    var b = /[a-zA-Z]/;
+
+  if(a<0){
+       var a=(x+x)%2
+     return a;
+    }else if(b.test(x)){
+       a="b"
+    }
+   else{
+        var a=x%2
+    }
+
+    
+     if(x===''){
+        y.innerHTML='Enter the correct value'
+     }
+    
+    
+    else if(a==0){
+        y.innerHTML=x+' is even';
+    }
+    else if(a<1){
+        y.innerHTML=x+'This is not a integer';
+    } else if(a="b"){
+        y.innerHTML=x+" "+'This is charactor';
+    }
+else {
+   y.innerHTML=x+''+' it is odd '
+}
+var x = document.getElementById('in').value=null;
+    }
+    
+
